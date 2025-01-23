87461994338f5435a7439d075fe32124bc1e52c2
faizanshabir123 <faizanshabir69@gmail.com>
2025-01-23 23:23:12 +0500

    Create 509. Fibonacci Number.py

---
+++ 509. Fibonacci Number/509. Fibonacci Number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        if(n==0 or n==1):
+            return n
+        
+        a=0
+        b=1
+
+        for i in range(1,n):
+            a,b=b,a+b
+        
+        return b
